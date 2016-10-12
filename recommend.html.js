423b50c8703e41f32c45042e24e58cfae889a599
LS--20160803ISH\Administrator <897622361@qq.com>
2016-10-12 16:49:18 +0800

    1232

---
+++ recommend.html.js
@@ -0,0 +1,79 @@
+/*
+* @Author: Administrator
+* @Date:   2016-10-12 16:21:05
+* @Last Modified by:   Administrator
+* @Last Modified time: 2016-10-12 16:21:32
+*/
+
+var recoommend =`
+<div class="rm1_banner">
+<img src="http://img-home.49app.com/wap/platfor//images/topbian02.jpg" width="100%">
+<img src="http://img-home.49app.com./Uploads/2016-04/571045df90a5d.jpg" width="100%">
+</div>
+<div class="rm2_banner">
+<a href="javascript:void(0)" class="btn-lblq f1" onclick="">
+  <img src="http://img-home.49app.com/wap/platfor/images/lqlbtn.png">
+</a>
+<a href="javascript:void(0)" class="btn-yxxz f1" onclick="">
+  <img src="http://img-home.49app.com/wap/platfor/images/yxxzbtn.png">
+</a>
+</div>
+<div class="rm3_banner" style="height: auto">
+  《葫芦娃》是一款横版动作策略手游，游戏根据经典国产动画《葫芦兄弟》改编，再现最真实的童年记忆。游戏挥别传统AI组队，策略搭配默契组合，与游戏好友实时互动，带领葫芦七兄弟勇闯妖巢维护正义，百余关卡等你来战。原汁原味的角色造型，精度还原的故事情节，都将唤醒你内心深处的童年梦.......妖怪，还我爷爷!还我爷爷!
+</div>
+<!--    <div class="mui-content">
+  <ul class="mui-table-view mui-table-view-chevron">
+    <li id="switch" class="mui-table-view-cell">
+      定时轮播
+      <div class=" mui-active">
+        <div class="mui-switch-handle"></div>
+      </div>
+    </li>
+  </ul>
+</div> -->
+        <li>
+        <div class="mui-collapse-content">
+          <div id="slider" class="mui-slider">
+            <div class="mui-slider-group mui-slider-loop">
+              <!-- 额外增加的一个节点(循环轮播：第一个节点是最后一张轮播) -->
+              <div class="mui-slider-item mui-slider-item-duplicate">
+                <a href="#">
+                  <img src="img/571045bb65c44.jpg">
+                </a>
+              </div>
+              <!-- 第一张 -->
+              <div class="mui-slider-item">
+                <a href="#">
+                  <img src="img/571045a81d88c.jpg">
+                </a>
+              </div>
+              <!-- 第二张 -->
+              <div class="mui-slider-item">
+                <a href="#">
+                  <img src="img/571045b1d98e9.jpg">
+                </a>
+              </div>
+              <!-- 第三张 -->
+              <div class="mui-slider-item">
+                <a href="#">
+                  <img src="img/571045bb65c44.jpg">
+                </a>
+              </div>
+              <!-- 第四张 -->
+
+              <!-- 额外增加的一个节点(循环轮播：最后一个节点是第一张轮播) -->
+              <div class="mui-slider-item mui-slider-item-duplicate">
+                <a href="#">
+                  <img src="img/571045a81d88c.jpg">
+                </a>
+              </div>
+            </div>
+            <div class="mui-slider-indicator">
+              <div class="mui-indicator mui-active"></div>
+              <div class="mui-indicator"></div>
+              <div class="mui-indicator"></div>
+            </div>
+          </div>
+        </div>
+      </li>
+`;
