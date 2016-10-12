0c6bd48c66b027bdb0b24357095fb65fd6cde132
LS--20160803ISH\Administrator <897622361@qq.com>
2016-10-12 15:59:09 +0800

    page-hash

---
+++ header.es5.js
@@ -0,0 +1,1 @@
+var headerR = "\n<a class=\"mui-action-back mui-icon mui-icon-left-nav mui-pull-left\"></a>\n<h1 class=\"mui-title\">注册登陆</h1>\n";
---
+++ mainHead.html.js
@@ -0,0 +1,21 @@
+/*
+* @Author: Administrator
+* @Date:   2016-10-12 15:07:34
+* @Last Modified by:   Administrator
+* @Last Modified time: 2016-10-12 15:08:42
+*/
+
+var mainHead =`
+<a class="mui-pull-left logo_sy db pa">
+  <img src="images/54ab7990ba321.png" >
+</a>
+<div class="userdiv pa">
+  <a class="mui-icon mui-icon-contact mui-tab-item" href="#userCenter">
+
+  </a>
+  <a class="mui-tab-item pa bl userName customColor" href="#userCenter">
+    <span class="bl rel conHeight">用户中心</span>
+  </a>
+
+</div>
+`;
