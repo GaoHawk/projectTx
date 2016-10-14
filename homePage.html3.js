8bfb4dfd3c5c16d14bb437b9edbb5bda3231d5ac
LS--20160803ISH\Administrator <897622361@qq.com>
2016-10-14 16:01:02 +0800

    12335

---
+++ homePage.html3.js
@@ -0,0 +1,119 @@
+var html3 =`
+<div class="mui-content">
+
+</div>
+<div id="slider" class="mui-slider" >
+  <div class="mui-slider-group mui-slider-loop">
+    <!-- 额外增加的一个节点(循环轮播：第一个节点是最后一张轮播) -->
+    <div class="mui-slider-item mui-slider-item-duplicate">
+      <a href="#">
+        <img src="images/yuantiao.jpg">
+      </a>
+    </div>
+    <!-- 第一张 -->
+    <div class="mui-slider-item">
+      <a href="#">
+        <img src="images/shuijiao.jpg">
+      </a>
+    </div>
+    <!-- 第二张 -->
+    <div class="mui-slider-item">
+      <a href="#">
+        <img src="images/muwu.jpg">
+      </a>
+    </div>
+    <!-- 第三张 -->
+    <div class="mui-slider-item">
+      <a href="#">
+        <img src="images/cbd.jpg">
+      </a>
+    </div>
+    <!-- 第四张 -->
+    <div class="mui-slider-item">
+      <a href="#">
+        <img src="images/yuantiao.jpg">
+      </a>
+    </div>
+    <!-- 额外增加的一个节点(循环轮播：最后一个节点是第一张轮播) -->
+    <div class="mui-slider-item mui-slider-item-duplicate">
+      <a href="#">
+        <img src="images/shuijiao.jpg">
+      </a>
+    </div>
+  </div>
+  <div class="mui-slider-indicator">
+    <div class="mui-indicator mui-active"></div>
+    <div class="mui-indicator"></div>
+    <div class="mui-indicator"></div>
+    <div class="mui-indicator"></div>
+  </div>
+</div>
+
+<div style="text-align:center;margin-top: 12px; ">
+  <span class="" style="display: inline-block; width: 75%;   border-radius: 6px;border: 1px solid #6ddeff">
+   <a href="#" >
+    <img src="images/49you.png" style="width: 64px;height: 64px;">
+   </a>
+   <p style="display: inline-block;width: 50%;padding-left: 15px;">
+    <span style="font-size: 16px;color: black;">手机游戏盒子</span><br>
+    更多礼包
+    免费游戏下载领取
+   </p>
+
+  <a href="#">
+    <img src="images/dlimage.png" style="width:45px;height:45px;" >
+  </a>
+  </span>
+</div>
+
+<div id="navi">
+    <ul style="padding-left: 0;">
+      <li>
+        <a href="#" class="myButton custom-navBtn">
+
+          <span>魔域之剑</span>
+          <button style="
+              font-size: 12px;
+              width: 100%;
+              padding: 2px 1px;
+          ">下载游戏</button>
+        </a>
+      </li>
+      <li>
+        <a href="#" class="myButton custom-navBtn">
+
+          <span>魔域之剑</span>
+          <button style="
+              font-size: 12px;
+              width: 100%;
+              padding: 2px 1px;
+          ">下载游戏</button>
+        </a>
+      </li>
+      <li>
+        <a href="#" class="myButton custom-navBtn">
+
+          <span>魔域之剑</span>
+          <button style="
+              font-size: 12px;
+              width: 100%;
+              padding: 2px 1px;
+          ">下载游戏</button>
+        </a>
+      </li>
+      <li>
+        <a href="#" class="myButton custom-navBtn">
+
+          <span>魔域之剑</span>
+          <button style="
+              font-size: 12px;
+              width: 100%;
+              padding: 2px 1px;
+          ">下载游戏</button>
+        </a>
+      </li>
+
+
+    </ul>
+  </div>
+`;
