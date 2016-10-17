fcf920d1287e26a57849f4ede159776c611206a4
LS--20160803ISH\Administrator <897622361@qq.com>
2016-10-17 15:25:16 +0800

    xiangg  js

---
+++ consult.html.js
@@ -0,0 +1,102 @@
+var consult=`
+<div class="mui-collapse-content">
+							<div id="slider" class="mui-slider">
+								<div class="mui-slider-group mui-slider-loop">
+									<!-- 额外增加的一个节点(循环轮播：第一个节点是最后一张轮播) -->
+									<div class="mui-slider-item mui-slider-item-duplicate">
+										<a href="#" class="no-tab">
+											<img src="images/home-slide/sli11.png">
+										</a>
+									</div>
+									<!-- 第一张 -->
+									<div class="mui-slider-item">
+										<a href="#" class="no-tab">
+											<img src="images/home-slide/sli10.jpg">
+										</a>
+									</div>
+									<!-- 第二张 -->
+									<div class="mui-slider-item">
+										<a href="#" class="no-tab">
+											<img src="images/home-slide/home-slide2.png">
+										</a>
+									</div>
+									<!-- 第三张 -->
+									<div class="mui-slider-item">
+										<a href="#" class="no-tab">
+											<img src="images/home-slide/home-slide3.png">
+										</a>
+									</div>
+									<!-- 第四张 -->
+									<div class="mui-slider-item">
+										<a href="#" class="no-tab">
+											<img src="images/home-slide/home-slide4.png">
+										</a>
+									</div>
+									<!-- 第五张 -->
+									<div class="mui-slider-item">
+										<a href="#" class="no-tab">
+											<img src="images/home-slide/sli11.png">
+										</a>
+									</div>
+									<!-- 额外增加的一个节点(循环轮播：最后一个节点是第一张轮播) -->
+									<div class="mui-slider-item mui-slider-item-duplicate">
+										<a href="#" class="no-tab">
+											<img src="images/home-slide/sli10.jpg">
+										</a>
+									</div>
+								</div>
+								<div class="mui-slider-indicator">
+									<div class="mui-indicator mui-active"></div>
+									<div class="mui-indicator"></div>
+									<div class="mui-indicator"></div>
+									<div class="mui-indicator"></div>
+									<div class="mui-indicator"></div>
+								</div>
+							</div>
+						</div>
+		<main>
+			<div class="zx">
+				<img src="http://img-home.49app.com/wap/platfor//images/listicon.png" height="18">
+				资讯活动
+			</div>
+			<ul class="mui-table-view">
+				 <li class="mui-table-view-cell">
+				 	<a href="#tabbar-childPage" class="mui-tab-item news-a" style="display: inline-block">
+				 		《九天仙境》192服“轻风听雨”10月13日10：00火爆开启！！！
+				 	</a>
+				 	<span class="datespan">10.13</span>
+				 </li>
+				 <li class="mui-table-view-cell">
+				 	<a href="#tabbar-childPage" class="mui-tab-item news-a" style="display: inline-block">
+				 		《仙剑神域》310服“殢云尤雨”10月14日火爆开启！！
+				 	</a>
+				 	<span class="datespan">10.13</span>
+				 </li>
+				 </li>
+				 <li class="mui-table-view-cell">
+				 	<a href="#tabbar-childPage" class="mui-tab-item news-a" style="display: inline-block">
+				 		《九天仙境》193服“水榭听香”10月14日10：00火爆开启！！！
+				 	</a>
+				 	<span class="datespan">10.13</span>
+				 </li>
+				 </li>
+				 <li class="mui-table-view-cell">
+				 	<a href="#tabbar-childPage" class="mui-tab-item news-a" style="display: inline-block">
+				 		《弑天刃》62服“宣化承流”10月14日10:00 火爆开启！！！
+				 	</a>
+				 	<span class="datespan">10.13</span>
+				 </li>
+				 </li>
+				 <li class="mui-table-view-cell">
+				 	<a href="#tabbar-childPage" class="mui-tab-item news-a" style="display: inline-block">
+				 		《洛神域》公测355服“贪狼破军”10月14日10:00开启！
+				 	</a>
+				 	<span class="datespan">10.13</span>
+				 </li>
+				 </li>
+			</ul>
+			<a href="javascript:void(0);" class="zx-more" id="load_more" style="padding: 25px;margin-bottom: 34%;width: 298px;height: 94px;margin: 5% auto 0;text-align: center;color: #666;font-size: 14px;line-height: 33px;border-radius: 32px;border: 1px solid #cbcbcb;box-shadow: 0 1px 2px #cbcbcb;display: block;font-size: 43px;"">查看更多</a>
+		<!-- 	<input type="button" class="tttt" value="type=button" /> -->
+
+		</main>
+`;
